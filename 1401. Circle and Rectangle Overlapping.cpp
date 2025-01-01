b144b56c68326cb3299dca226c9446b721091418
Npal21 <nbpal21@gmail.com>
2025-01-01 17:25:45 +0530

    Created Circle and Rectangle Overlapping.cpp

---
+++ 1401. Circle and Rectangle Overlapping.cpp
@@ -0,0 +1,19 @@
+//Link: https://leetcode.com/problems/circle-and-rectangle-overlapping/
+
+class Solution {
+public:
+    bool checkOverlap(int radius, int xCenter, int yCenter, int x1, int y1, int x2, int y2) {
+
+        auto f = [](int part1, int part2, int partCenter) -> int{
+            if(part1 <= partCenter && partCenter <= part2){
+                return 0;
+            }
+            return partCenter<part1 ? part1-partCenter : partCenter-part2;
+        };
+
+        int a = f(x1, x2, xCenter);
+        int b = f(y1, y2, yCenter);
+        return a*a + b*b <= radius*radius;
+        
+    }
+};
