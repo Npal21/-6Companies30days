0c7c281926b38097a05aa8751548c1b950f646d7
Npal21 <nbpal21@gmail.com>
2025-01-23 00:34:31 +0530

    Created Letter Combinations of a Phone Number

---
+++ 17. Letter Combinations of a Phone Number.cpp
@@ -0,0 +1,24 @@
+
+//Link: https://leetcode.com/problems/letter-combinations-of-a-phone-number/
+
+class Solution {
+public:
+    vector<string> letterCombinations(string digits) {
+        if (digits.empty()) {
+            return {};
+        }
+        vector<string> d = {"abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        vector<string> ans = {""};
+        for (auto& i : digits) {
+            string s = d[i - '2'];
+            vector<string> t;
+            for (auto& a : ans) {
+                for (auto& b : s) {
+                    t.push_back(a + b);
+                }
+            }
+            ans = move(t);
+        }
+        return ans;
+    }
+};
